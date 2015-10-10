eef3f65554f56e913d137bfc1a69e295fbfa904f
Manny Batule <mfbatule2@gmail.com>
2015-10-10 19:45:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Aliai
+=====
+
+Automatically configures an alias file for you.  Written in Ruby and very much a work in progress.
---
+++ lib/aliai.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+#
+# Main class
+#
+
+require "./profile"
+
+profile = Profile.new()
+puts profile.profileExists?
+
